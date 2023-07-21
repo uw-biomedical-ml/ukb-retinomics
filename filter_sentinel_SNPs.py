3b2d9a4470457976a3bca2a7f4e92fe64dc8ffff
LAPTOP-3NGVR33I\yue <hypallage@gmail.com>
2023-07-21 14:28:28 -0700

    updated file structure

--- filter_sentinel_SNPs.py
+++ filter_sentinel_SNPs.py
@@ -1,7 +1,7 @@
 import os, json
 from sys import platform, float_info
 
-from .transform_gwas_to_long import file_sep, DATA_FOLDER, NUM_COORDS, NUM_COORDS_VALID, NUM_SLICES, NUM_SLICES_VALID, \
+from transform_gwas_to_long import file_sep, DATA_FOLDER, NUM_COORDS, NUM_COORDS_VALID, NUM_SLICES, NUM_SLICES_VALID, \
     SNIP_COUNTS, NUM_SNIPS, CHROMOSOMES, SLICE_NUMS, COORD_NUMS, safe_snipID
 
 
@@ -110,7 +110,139 @@ def filter_SNPs(chromosomes=CHROMOSOMES):
     return
 
 
+def make_combined_SNPs_info_file():
+    data_folder_html = os.path.join("html", "data", "gwas")
+    data_folder_raw = os.path.join("data_raw", "gwas")
+    # locSNPIDs_file = os.path.join(data_folder_html, "gwSigLociSNP_IDs.csv")
+    # locSNPIDs = []
+    # with open(locSNPIDs_file, "r") as fin:
+    #     for l in fin:
+    #         locSNPIDs.append(l.rstrip())
+    # fin.close()
+    # locSNPIDs_set = set(locSNPIDs)  # NB - non-unique by just rs_name!
+
+    locSNP_file = os.path.join(data_folder_html, "gwSigLociSNPs.csv")
+    sigSNPs_dict = {}
+    with open(locSNP_file, "r") as fin:
+        counter = 0
+        for l in fin:
+            counter +=1
+            l_toks = l.rstrip().split(",")
+            if counter==1:
+                header=l_toks  # locusID,sentinelSNPID,locusSNPID,r2withSentinel
+            else:
+                cur_dict = dict(zip(header, l_toks))
+                curID = cur_dict["locusSNPID"]
+                sigSNPs_dict[curID] = cur_dict
+    fin.close()
+    locSNPIDs_set = set(sigSNPs_dict.keys())
+
+    sentinelSNPs_file = os.path.join(data_folder_html, "gwSigLociSummary.csv")
+    sentinelDict = {}
+    with open(sentinelSNPs_file, "r") as fin:
+        counter = 0
+        for l in fin:
+            counter +=1
+            l_toks = l.rstrip().split(",")
+            if counter==1:
+                header=l_toks
+            else:
+                cur_dict = dict(zip(header, l_toks))
+                curID = cur_dict["ID"]
+                sentinelDict[curID] = cur_dict
+    fin.close()
+
+    # go through slice1_result.txt for each chromosome
+    sigSNPS_outfile = os.path.join(data_folder_html, "info_sigSNPs.csv")
+    SNPS_outfile = os.path.join(data_folder_html, "info_SNPs.csv")
+    with open(sigSNPS_outfile, "w") as fout_sig:
+        fout_sig.write("SNP,chr,pos,A1,num_sig,bonf\n")
+        with open(SNPS_outfile, "w") as fout:
+            # fout.write("SNP,chr,pos,A1\n")
+            fout.write("SNP,chr\n")    # bare minimum
+
+            for cdx, chromosome in enumerate(CHROMOSOMES):
+                fpath = os.path.join(data_folder_raw, "chr{}".format(chromosome), "slice1_result.txt")
+                print("processing ", cdx, chromosome, fpath)
+
+                with open(fpath, "r") as fin:
+                    counter = 0
+                    for l in fin:
+                        counter += 1
+                        l_toks = l.rstrip().split(",")
+
+                        if counter==1:
+                            header = l_toks
+                        else:
+                            cur_dict = dict(zip(header, l_toks))
+                            curID = cur_dict["ID"]
+                            if curID in locSNPIDs_set:
+                                sentinel_data = sentinelDict[sigSNPs_dict[curID]["sentinelSNPID"]]
+                                out_vals = [curID, chromosome, cur_dict["POS"], cur_dict["A1"], sentinel_data["nPixelsLocus"], sentinel_data["BonferroniSig"]]
+                                fout_sig.write("{}\n".format(",".join([str(x) for x in out_vals])))
+                            else:
+                                out_vals = [curID, chromosome]  # bare minimum
+                                fout.write("{}\n".format(",".join([str(x) for x in out_vals])))
+                fin.close()
+        fout.close()
+    fout_sig.close()
+
+    return
+
+
+def make_combined_location_files():
+    data_folder_html = os.path.join("html", "data", "gwas")
+    location_folder = os.path.join(data_folder_html, "location_summary")
+    if not os.path.isdir(location_folder):
+        os.makedirs(location_folder)
+
+    for cdx, chromosome in enumerate(CHROMOSOMES):
+        slice_coord_dict = {}
+        chr_folder = os.path.join(data_folder_html, "chr{}".format(chromosome), "sigSNPs", "long")
+        for sdx, slice_num in enumerate(range(1,NUM_SLICES_VALID+1)):
+            fpath = os.path.join(chr_folder, "slice{}.txt".format(slice_num))
+            print("processing: ", cdx, chromosome, sdx, slice_num, fpath)
+
+            with open(fpath, "r") as fin:
+                counter = 0
+                for l in fin:
+                    counter+=1
+                    l_toks = l.rstrip().split(",")
+
+                    if counter==1:
+                        header = l_toks
+                        header = ["SNP","slice_num","coord_num","beta","pval","Bonf"]
+                    else:
+                        cur_dict = dict(zip(header, l_toks))
+                        slice_coord = "{},{}".format(cur_dict["slice_num"],cur_dict["coord_num"])
+                        if slice_coord not in slice_coord_dict:
+                            slice_coord_dict[slice_coord] = []
+                        cur_vals = [cur_dict["SNP"],chromosome,cur_dict["beta"],cur_dict["pval"]]
+                        slice_coord_dict[slice_coord].append(cur_vals)
+            fin.close()
+
+        # write location_summaries at chromosome level to avoid RAM issues
+        for slice_coord, sdata in slice_coord_dict.items():
+            stoks = slice_coord.split(",")
+            slice_num, coord_num = stoks
+            outpath = os.path.join(location_folder, "loc_summary_{}_{}.csv".format(slice_num, coord_num))
+            print("writing: ", cdx, chromosome, slice_num, coord_num, outpath)
+
+            if not os.path.isfile(outpath):     # add header if first write
+                with open(outpath, "w") as fout:
+                    fout.write("SNP,chr,beta,pval\n")
+                fout.close()
+
+            with open(outpath, "a") as fout:    # otherwise append
+                for ddx, d in enumerate(sdata):
+                    fout.write("{}\n".format(",".join([str(x) for x in d])))
+            fout.close()
+    return
+
+
 if __name__ == "__main__":
     # parse_sentinelSNPs()
     # parse_lociSNPs()
-    filter_SNPs()
+    # filter_SNPs()
+    # make_combined_SNPs_info_file()
+    make_combined_location_files()
